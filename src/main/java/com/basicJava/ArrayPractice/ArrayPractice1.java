fe551019307d686cdc58f08c3cc463ac7e423b7c
Glogic <rupali.bojewar@glogic.co.in>
2024-03-30 11:02:39 +0530

    one more practice file

---
+++ src/main/java/com/basicJava/ArrayPractice/ArrayPractice1.java
@@ -0,0 +1,10 @@
+package com.basicJava.ArrayPractice;
+
+public class ArrayPractice1 {
+    public static void main(String[] args) {
+        String[] s1={"Selenium","Test-complete","Playwright"};
+        for (String s:s1) {
+            System.out.println(s );
+        }
+    }
+}
